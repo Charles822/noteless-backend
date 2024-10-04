b8718cf7435b1e17704f8e00d752386637801836
Charles Fauchet <charles.fauchet@gmail.com>
2024-10-04 09:03:35 +0800

    Modified migration issue with Video model

--- contents/migrations/0002_auto_20240722_0603.py
+++ contents/migrations/0002_auto_20240722_0603.py
@@ -10,9 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='video',
-            name='tags',
-            field=models.JSONField(null=True, blank=True),
-        ),
     ]
