8ffcc1b6a14620260e46788f37dae908cb520e98
Charles Fauchet <charles.fauchet@gmail.com>
2024-10-10 16:58:43 +0900

    Re adding redis conf for production

--- htfbi_backend/celery.py
+++ htfbi_backend/celery.py
@@ -8,16 +8,16 @@ app = Celery('htfbi_backend')
 
 
 
-# app.conf.update(
-#     broker_use_ssl={
-#         'ssl_cert_reqs': ssl.CERT_NONE
-#     },
-#     redis_backend_use_ssl={
-#         'ssl_cert_reqs': ssl.CERT_NONE
-#     }
-# )
+app.conf.update(
+    broker_use_ssl={
+        'ssl_cert_reqs': ssl.CERT_NONE
+    },
+    redis_backend_use_ssl={
+        'ssl_cert_reqs': ssl.CERT_NONE
+    }
+)
 
-# app.conf.broker_connection_retry_on_startup = True # silence warning: broker connection retries are made during startup
+app.conf.broker_connection_retry_on_startup = True # silence warning: broker connection retries are made during startup
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
