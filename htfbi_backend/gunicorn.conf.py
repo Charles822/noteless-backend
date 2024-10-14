3c382acfa508996793f5d144901f99534d004221
Charles Fauchet <charles.fauchet@gmail.com>
2024-10-14 11:17:31 +0800

    Removed timeout from gunicorn config

--- htfbi_backend/gunicorn.conf.py
+++ htfbi_backend/gunicorn.conf.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 workers = 2
 worker_class = 'gevent'  # or 'sync' if async is not needed
-timeout = 60
+# timeout = 60
 # bind = "0.0.0.0:8000"
 accesslog = '-'
 errorlog = '-'
